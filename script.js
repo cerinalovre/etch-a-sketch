4a3a6f28c2610a2d1b1d87d18f882158df397266
lovre <cerina.lovre@gmail.com>
2022-08-02 11:42:48 +0200

    add eraser tool

--- script.js
+++ script.js
@@ -1,59 +1,63 @@
-function main() {
-  const n = 64;
-  const container = document.querySelector(".drawing-area");
-  const colorInput = document.querySelector("#color");
-  let color = "#000";
-  let mouseDown = false;
+const n = 64;
+const container = document.querySelector(".drawing-area");
+const colorInput = document.querySelector("#color");
+const eraser = document.querySelector(".eraser");
+let color = "#DC3D3D";
+let mouseDown = false;
 
-  colorInput.addEventListener("input", () => {
-    console.log(colorInput.value);
-    color = colorInput.value;
-  });
+colorInput.addEventListener("input", () => {
+  console.log(colorInput.value);
+  color = colorInput.value;
+});
 
-  container.addEventListener("mouseleave", () => {
-    mouseDown = false;
-  });
+container.addEventListener("mouseleave", () => {
+  mouseDown = false;
+});
+
+eraser.addEventListener("click", () => {
+  color = "#FFFFFF";
+});
+
+for (let i = 0; i < n; i++) {
+  const row = document.createElement("div");
+  container.appendChild(row);
+  row.classList.add("row");
 
   for (let i = 0; i < n; i++) {
-    const row = document.createElement("div");
-    container.appendChild(row);
-    row.classList.add("row");
-
-    for (let i = 0; i < n; i++) {
-      const pixel = document.createElement("div");
-      pixel.classList.add("pixel");
-      pixel.setAttribute("draggable", "false");
-      row.appendChild(pixel);
-
-      pixel.addEventListener("mousedown", () => {
-        mouseDown = true;
-        console.log(mouseDown);
-      });
-
-      pixel.addEventListener("mouseup", () => {
-        mouseDown = false;
-        console.log(mouseDown);
-      });
-
-      pixel.addEventListener("mousemove", () => {
-        if (mouseDown) {
-          pixel.style.backgroundColor = color;
-        }
-      });
-
-      pixel.addEventListener("click", () => {
-        pixel.style.backgroundColor = color;
-      });
-
-      pixel.addEventListener("dragstart", (e) => {
-        e.preventDefault();
-      });
-
-      pixel.addEventListener("drop", (e) => {
-        e.preventDefault();
-      });
-    }
+    const pixel = document.createElement("div");
+    pixel.classList.add("pixel");
+    pixel.setAttribute("draggable", "false");
+    row.appendChild(pixel);
+    pixelEventListeners(pixel);
   }
 }
 
-main();
+function pixelEventListeners(pixel) {
+  pixel.addEventListener("mousedown", () => {
+    mouseDown = true;
+    console.log(mouseDown);
+  });
+
+  pixel.addEventListener("mouseup", () => {
+    mouseDown = false;
+    console.log(mouseDown);
+  });
+
+  pixel.addEventListener("mousemove", () => {
+    if (mouseDown) {
+      pixel.style.backgroundColor = color;
+    }
+  });
+
+  pixel.addEventListener("click", () => {
+    pixel.style.backgroundColor = color;
+  });
+
+  pixel.addEventListener("dragstart", (e) => {
+    e.preventDefault();
+  });
+
+  pixel.addEventListener("drop", (e) => {
+    e.preventDefault();
+  });
+}
